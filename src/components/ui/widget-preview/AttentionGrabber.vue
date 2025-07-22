faad0f845d608482662c27bc9f6c7160b033c80a
Ochaa <mushoddaqt@gmail.com>
2025-07-22 14:22:59 +0700

    feat(widget-preview): add toggle options for image and text display in AttentionGrabber component

--- src/components/ui/widget-preview/AttentionGrabber.vue
+++ src/components/ui/widget-preview/AttentionGrabber.vue
@@ -9,10 +9,14 @@ const props = withDefaults(
   defineProps<{
     imageUrl: string;
     title: string;
+    isImageEnable: boolean;
+    isTextEnable: boolean;
   }>(),
   {
     imageUrl: DEFAULT_IMAGE_PREVIEW.ATTENTION_GRABBER_IMAGE,
     description: 'Hello, there is Promo!',
+    isImageEnable: false,
+    isTextEnable: false,
   }
 );
 
@@ -23,18 +27,28 @@ const finalImageUrl = computed(() => {
 
 <template>
   <div
-    class="text-navy-500 shadow-card-float flex w-[360px] flex-col overflow-hidden rounded-4xl bg-white"
+    class="text-navy-500 shadow-card-float relative flex w-[360px] flex-col overflow-hidden rounded-4xl bg-white"
   >
     <div
-      class="bg-surface-disable relative grid h-[208px] max-h-[208px] min-h-[208px] w-full flex-1 place-items-center"
+      v-if="isImageEnable"
+      class="bg-surface-disable grid h-[208px] max-h-[208px] min-h-[208px] w-full flex-1 place-items-center"
     >
       <ButtonIcon v-show="imageUrl" class="absolute top-4 right-4 text-white">
         <CloseIcon />
       </ButtonIcon>
       <img :src="finalImageUrl" alt="image attention grabber" class="h-full w-full object-cover" />
     </div>
-    <div class="p-6 text-left break-words">
-      {{ title }}
+    <div v-if="isTextEnable" class="p-6 text-left break-words">
+      <ButtonIcon
+        v-if="!isImageEnable"
+        v-show="imageUrl"
+        class="text-text-title absolute top-4 right-4"
+      >
+        <CloseIcon />
+      </ButtonIcon>
+      <p class="w-9/10 break-words">
+        {{ title }}
+      </p>
     </div>
   </div>
 </template>
